e6fb3925831720c1d7ddbc3e5cece94fccd72aff
jacques <jacques>
2019-11-21 20:41:48 +0100

    environment ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@ const BASE_HEROKU = 'https://meannetsocial.herokuapp.com/';
 
 export const environment = {
   production: true,
-  chatUrl: BASE_HEROKU,
+  chatUrl: BASE_URL,
   backendUrl: `${BASE_URL}/api`,
   chatPath: `/mean-chat-app.io`,
 };
